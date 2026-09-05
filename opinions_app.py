415a20116c37a6a6c11b6a1e044a83d3c7231480
FatFedCat <baev.fedar@yandex.ru>
2026-09-05 22:37:16 +0300

    3

--- opinions_app.py
+++ opinions_app.py
@@ -4,12 +4,17 @@ from datetime import datetime
 from random import randrange
 
 # Импортировать функцию render_template().
-from flask import Flask, render_template
+from flask import Flask, abort, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length, Optional
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = 'I FATCAT'
 
 db = SQLAlchemy(app)
 
@@ -20,27 +25,78 @@ class Opinion(db.Model):
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
+class OpinionForm(FlaskForm):
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 128)]
+    )
+    text = TextAreaField(
+        'Напишите мнение', 
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()]
+    )
+    submit = SubmitField('Добавить')
+
 @app.route('/')
 def index_view():
     quantity = Opinion.query.count()
     if not quantity:
-        return 'В базе данных записей нет.'
+        # Если в базе пусто - при запросе к главной странице
+        # пользователь увидит ошибку 500.
+        abort(500)
     offset_value = randrange(quantity)
     opinion = Opinion.query.offset(offset_value).first()
-    # Подключить шаблон opinion.html.
-    return render_template('opinion.html', opinion=opinion)
+    return render_template('opinion.html', opinion=opinion) 
 
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
-    # Подключить шаблон add_opinion.html.
-    return render_template('add_opinion.html')
+    form = OpinionForm()
+    # Если ошибок не возникло...
+    if form.validate_on_submit():
+        # ...то нужно создать новый экземпляр класса Opinion...
+        opinion = Opinion(
+            # ...и передать в него данные, полученные из формы.
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
+        )
+        # Затем добавить его в сессию работы с базой данных...
+        db.session.add(opinion)
+        # ...и зафиксировать изменения.
+        db.session.commit()
+        # Затем переадресовать пользователя на страницу добавленного мнения.
+        return redirect(url_for('opinion_view', id=opinion.id))
+    # Если валидация не пройдена - просто отрисовать страницу с формой.
+    return render_template('add_opinion.html', form=form) 
 
 @app.route('/opinions/<int:id>') 
 def opinion_view(id):  
     # Метод get() заменён на get_or_404():
     opinion = Opinion.query.get_or_404(id)  
     return render_template('opinion.html', opinion=opinion) 
+
+@app.errorhandler(500)
+def internal_error(error):
+    # Ошибка 500 возникает в нештатных ситуациях на сервере. 
+    # Например, провалилась валидация данных.
+    # В таких случаях можно откатить изменения, незафиксированные в БД,
+    # чтобы в базу не записалось ничего лишнего.
+    db.session.rollback()
+    # Пользователю вернётся страница, сгенерированная на основе шаблона 500.html.
+    # Этого шаблона пока нет, но сейчас вы его тоже создадите.
+    # Пользователь получит и код HTTP-ответа 500.
+    return render_template('500.html'), 500
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # При ошибке 404 в качестве ответа вернётся страница, созданная
+    # на основе шаблона 404.html и код HTTP-ответа 404.
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run() 
